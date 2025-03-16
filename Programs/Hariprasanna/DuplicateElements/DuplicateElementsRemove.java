4c81688aec581bce637ddd99c8f7805c6ee69a41
Hariprasanna2003 <140971009+Hariprasanna2003@users.noreply.github.com>
2025-03-16 22:43:35 +0530

    DuplicateElementsRemove.java

--- Programs/Hariprasanna/DuplicateElements/DuplicateElementsRemove.java
+++ Programs/Hariprasanna/DuplicateElements/DuplicateElementsRemove.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class DuplicateElementsRemove{
+public class DuplicateElementsRemove {
 
-    public static void main(String[] args){
-        int[] originalArr = {10,9,6,2,8,3,7,5,5,6,6,7,7,7,8,8,9,9,10,10};
+    public static void main(String[] args) {
+        int[] originalArr = { 10, 9, 6, 2, 8, 3, 7, 5, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 10, 10 };
         List<Integer> duplicateRemoved = new ArrayList<>();
-        for(int i = 0; i < originalArr.length-1; i++){
-                if(!duplicateRemoved.contains(originalArr[i])){
-                    duplicateRemoved.add(originalArr[i]);           
-            }                      
-        }  
-        System.out.print("Duplicate elements removed: "+duplicateRemoved);       
+        for (int i : originalArr) {
+            if (!duplicateRemoved.contains(i)) {
+                duplicateRemoved.add(i);
+            }
+        }
+        System.out.print("Duplicate elements removed: " + duplicateRemoved);
     }
 }
